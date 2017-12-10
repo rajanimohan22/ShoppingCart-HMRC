535de008520b31cc25dae9493456606cc4f58a1c
rajanimohan22 <raomohan2006@yahoo.com>
2017-12-10 12:00:43 +0000

    Product List added and tested

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "ShoppingCart-HMRC"
 version := "1.0"
 
 scalaVersion := "2.11.7"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.5" % "test"
---
+++ src/main/scala/uk/gov/hmrc/shoppingcart/Product.scala
@@ -0,0 +1,8 @@
+package uk.gov.hmrc.shoppingcart
+
+/**
+  * Created by Mohan Dolla on 10/12/2017.
+  */
+sealed trait  Product
+case object Apple extends Product
+case object Orange extends Product
